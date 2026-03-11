ba8808dbd5ae6d04f041d154c5010634a34b244e
Karen-Anahi-Padron-Martinez <karenpadron0608@gmail.com>
2026-03-10 19:52:28 -0600

    fix:Correccion de CRUD Usuarios y uso de bcrypt

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { ApiOperation } from '@nestjs/swagger';
 
 @Controller('api/auth')
 export class AuthController {
@@ -9,4 +10,10 @@ export class AuthController {
   public login(): string {
     return this.authSvc.login();
   }
+
+  @Get("me")
+  @ApiOperation({ summary: "Extrae el ID del usuario desde el token y busca la informacion"})
+  public getProfile(){}
+
+  public refreshToken(){}
 }

b182f36f94abea32d72d3ba99b4cc877efa7db79
Karen-Anahi-Padron-Martinez <karenpadron0608@gmail.com>
2026-03-12 19:49:05 -0600

    fix: Configuracion de Login

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,15 +1,29 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body,Controller, Get, HttpException, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { ApiOperation } from '@nestjs/swagger';
+import { ExceptionsHandler } from '@nestjs/core/exceptions/exceptions-handler';
+import { LoginDto } from './dto/login';
+import { User } from '../user/entities/user.entity';
 
 @Controller('api/auth')
 export class AuthController {
   constructor(private authSvc: AuthService) {}
 
-  @Get()
-  public login(): string {
-    return this.authSvc.login();
-  }
+@Post('login')
+@ApiOperation({ summary: 'Login de usuario' })
+public async login(@Body() user: LoginDto): Promise<User> {
+
+    const result = await this.authSvc.login(user);
+
+    if (result == undefined || result == null) {
+        throw new HttpException(
+            `Usuario o contraseña incorrectos`,
+            HttpStatus.UNAUTHORIZED,
+        );
+    }
+
+    return result;
+}
 
   @Get("me")
   @ApiOperation({ summary: "Extrae el ID del usuario desde el token y busca la informacion"})
